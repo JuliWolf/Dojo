0fac45b3d2890e4d11a1bef9a66bf76f6ab02680
JuliWolf <qwert0038@yandex.ru>
2020-08-14 11:10:15 +0300

    delete Event Handling

--- 2. EventHandling/index.js
+++ 2. EventHandling/index.js
@@ -1,24 +1,60 @@
-require(["dojo/dom", "dojo/on", "dojo/domReady!"], function(dom, on){
-    var showButton = dom.byId("showDiv");
-    var hideButton = dom.byId("hideDiv");
-    var div = dom.byId("myDiv");
+require(["dojo/dom", "dojo/on", "dojo/mouse", "dojo/dom-style", "dojo/domReady!"],
+    function(dom, on, mouse, domStyle){
+        var showButton = dom.byId("showDiv");
+        var hideButton = dom.byId("hideDiv");
+        var div = dom.byId("myDiv");
+        var domStyleDiv = dom.byId("domStyle");
 
 
-    on(div, "mouseenter", function(event){
-        div.innerHTML = "Move mouse pointer away to change color to white";
-        div.style.background = "yellow";
-    });
+        // on(div, "mouseenter", function(event){
+        //     div.innerHTML = "Move mouse pointer away to change color to white";
+        //     div.style.background = "yellow";
+        // });
 
-    on(div, "mouseleave", function(event){
-        div.style.background = "white";
-        div.innerHTML = "Mouse over to change my background to yellow";
-    });
+        // on(div, "mouseleave", function(event){
+        //     div.style.background = "white";
+        //     div.innerHTML = "Mouse over to change my background to yellow";
+        // });
 
-    on(hideButton, "click", function(event){
-        div.style.display = "none";
-    });
 
-    on(showButton, "click", function(event){
-        div.style.display = "block";
-    });
-})
+        // on(hideButton, "click", function(event){
+        //     div.style.display = "none";
+        // });
+
+        //Handle only once
+        on.once(showButton, "click", function(event){
+            if(div.style.display == "none")
+                div.style.display = "block";
+
+            var styles = domStyle.getComputedStyle(domStyleDiv);
+            domStyleDiv.innerHTML = "Width id " + styles.width + " height is " + styles.height;
+        });
+
+        // With dojo/mouse
+        var mouseEnterHandle = on(div, mouse.enter, function(event){
+            domStyle.set(div, "width", "250px");
+            div.innerHTML = "Move mouse pointer away to change color to white; width is " +
+            domStyle.get(div, "width");
+            div.style.background = "yellow";
+            // Remove handler
+            mouseEnterHandle.remove();
+        });
+
+        on(div, mouse.leave, function(event){
+            div.style.background = "white";
+            div.innerHTML = "Mouse over to change my background to yellow";
+        });
+
+        on(hideButton, "mousedown", function(event){
+            if(mouse.isLeft(event)){
+                alert('mouse left button clicked');
+                div.style.display = "none";
+            }else if(mouse.isRight(event)){
+                alert('mouse right button clicked');
+            }
+        });
+
+
+
+    }
+)
